9088e212a82b77cb5443e2de953ac207aaad89d3
jsuci <jsuci.jsuci@gmail.com>
2022-12-13 02:36:17 +0800

    test gsm module at commands

--- raspberry_pi/gsm_module.py
+++ raspberry_pi/gsm_module.py
@@ -1,14 +1,35 @@
 import serial
 from time import sleep
 
-# Open a serial connection to a device with AT commands
-ser = serial.Serial("/dev/ttyS0", baudrate=115200, timeout=1)
 
-# Send an AT command and read the response
-ser.write(b'AT+GSN\r\n')
-sleep(5)
-response = ser.readlines()
-print(response)  # [b'\r\n', b'869988016370054\r\n', b'\r\n', b'OK\r\n']
+def at_command_exec(ser, com):
 
-# Close the serial connection
-ser.close()
+
+  # Send an AT command and read the response
+  ser.write(f'{com}\r\n'.encode())
+  sleep(5)
+  
+  # [b'\r\n', b'869988016370054\r\n', b'\r\n', b'OK\r\n']
+  response = ser.readlines()
+  
+  return response
+  
+  
+def main():
+  # Open a serial connection to a device with AT commands
+  ser = serial.Serial("/dev/ttyS0", baudrate=115200, timeout=1)
+  
+  # disable echo
+  print(at_command_exec(ser, 'ATE0'))
+  
+  # product serial number
+  print(at_command_exec(ser, 'AT+GSN'))
+  
+  # Close the serial connection
+  ser.close()
+  
+  
+if __name__ == "__main__":
+  main()
+  
+  
