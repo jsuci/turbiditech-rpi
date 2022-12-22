71dbda151637f7c2ebd1d6017264096e2d8a2736
jsuci <jsuci.jsuci@gmail.com>
2022-12-22 18:25:19 +0800

    organize files

--- raspberry_pi/misc/scripts/gsm_module.py
+++ raspberry_pi/misc/scripts/gsm_module.py
@@ -75,19 +75,55 @@ def http_get(ser):
   print(ser.readlines(), 'AT+HTTPTERM')
   
 
+
+def activate_ppp(ser):
+
+  # Send the AT command to check the module's response
+  ser.write(b'AT\r\n')
+  response = ser.read(1024).decode()
+  print(response)
+
+  # Set the APN
+  ser.write(b'AT+CGDCONT=1,"IP","http.globe.com.ph"\r\n')
+  response = ser.read(1024).decode()
+  print(response)
+
+  # Set the module to transparent transmission mode
+  ser.write(b'AT+CIPMODE=1\r\n')
+  response = ser.read(1024).decode()
+  print(response)
+
+  # Disable IP header compression
+  ser.write(b'AT+CIPCCFG=1,0,0,0,0,0\r\n')
+  response = ser.read(1024).decode()
+  print(response)
+
+  # Shut down previous connection, if any
+  ser.write(b'AT+CIPSHUT\r\n')
+  response = ser.read(1024).decode()
+  print(response)
+
+  # Establish a PPP connection
+  ser.write(b'ATD*99***1#\r\n')
+  response = ser.read(1024).decode()
+  print(response)
+
+  # Check the status of the connection
+  ser.write(b'AT+CPAS\r\n')
+  response = ser.read(1024).decode()
+  print(response)
+
+
   
   
 def main():
   # Open a serial connection to a device with AT commands
   ser = serial.Serial("/dev/ttyS0", baudrate=115200, timeout=1)
   
-  activate_gprs(ser)
-  
-  http_get(ser)
-  
+  activate_ppp(ser)
 
   # Close the serial connection
-  ser.close()
+  # ser.close()
   
   
 if __name__ == "__main__":
