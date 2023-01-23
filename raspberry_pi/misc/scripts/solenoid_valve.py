4b8f797ac95006fae40514f428bcc97c3a6d93f6
jsuci <jsuci.jsuci@gmail.com>
2023-01-23 08:39:05 +0800

    fix solenoid valve pin identification

--- raspberry_pi/misc/scripts/solenoid_valve.py
+++ raspberry_pi/misc/scripts/solenoid_valve.py
@@ -1,16 +1,9 @@
-#from gpiozero import DigitalOutputDevice
-#from time import sleep
-#
-#valve = DigitalOutputDevice(14)
-
 import OPi.GPIO as GPIO
 from time import sleep
 
-# Set the pin numbering mode
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-
-# Set pin 12 as an output pin
-GPIO.setup(11, GPIO.OUT)
+GPIO.setup(12, GPIO.OUT)
 
 
 
@@ -18,15 +11,15 @@ GPIO.setup(11, GPIO.OUT)
 
 while True:
   print('ON valve')
-  GPIO.output(11, GPIO.HIGH)
-  print('The current value of the pin is', GPIO.input(11))
+  GPIO.output(12, GPIO.HIGH)
+  print('The current value of the pin is', GPIO.input(12))
   print('sleep 5s\n\n')
   sleep(5)
 
 
   print('OFF valve')
-  GPIO.output(11, GPIO.LOW)
-  print('The current value of the pin is', GPIO.input(11))
+  GPIO.output(12, GPIO.LOW)
+  print('The current value of the pin is', GPIO.input(12))
   print('sleep 5s\n\n')
   sleep(5)
 
